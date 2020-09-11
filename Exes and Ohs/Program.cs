8797ae2434c23d83ff3cdd65d30a1807e326154d
PrikolnieSteni <70663287+PrikolnieSteni@users.noreply.github.com>
2020-09-12 01:18:23 +0300

    Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.(done)

---
+++ Exes and Ohs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Exes_and_Ohs
+{
+    class Program
+    {
+
+        public static bool XO(string input)
+        {
+            int[] values = { 0, 0 };
+            bool return_value = false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i].ToString().ToLower())
+                {
+                    case "x": values[0]++; break;
+                    case "o": values[1]++; break;
+                }
+            }
+            if(values[0] == values[1] || (values[0] == 0 && values[1] == 0))
+            {
+                return_value = true;
+            }
+            return return_value;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(XO("ooxx"));
+            Console.WriteLine(XO("xooxx"));
+            Console.WriteLine(XO("ooxXm"));
+            Console.WriteLine(XO("zpzpzpp"));
+            Console.WriteLine(XO("zzoo"));
+        }
+    }
+}
