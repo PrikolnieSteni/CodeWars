37f3767c6fb51cd51a4873f2adcb447262046bc4
PrikolnieSteni <70663287+PrikolnieSteni@users.noreply.github.com>
2020-09-12 16:09:10 +0300

    The main idea is to count all the occurring characters in a string.(done)

---
+++ Count characters in your string/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_characters_in_your_string
+{
+    class Program
+    {
+        public static Dictionary<char, int> Count(string str)
+        {
+            Dictionary<char, int> CountCharacters = new Dictionary<char, int>();
+            string new_str = default;
+            foreach (var value in str)
+            {
+                int count = str.Count(f => f == value);
+                new_str += value.ToString();
+                if (new_str.Count(f => f == value) <= 1)
+                {
+                    CountCharacters.Add(value, count);
+                }
+            }
+            return CountCharacters;
+            // return str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Count("aabbbbb"));
+        }
+    }
+}
