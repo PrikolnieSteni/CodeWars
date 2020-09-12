db2fd7e774cf092419a4231df0f3702ce1e95437
PrikolnieSteni <70663287+PrikolnieSteni@users.noreply.github.com>
2020-09-12 12:59:11 +0300

    Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.(done)

---
+++ Highest Scoring Word/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Highest_Scoring_Word
+{
+    class Program
+    {
+        private static string RemoveSpace(string value)
+        {
+            value = value.Replace(" ", "");
+            return value;
+        }
+        public static bool Feast(string beast, string dish)
+        {
+            if (RemoveSpace(beast).EndsWith(dish[dish.Length-1]) == true && RemoveSpace(beast).StartsWith(dish[0]) == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Feast("great blue heron", "garlic naan"));
+        }
+    }
+}
